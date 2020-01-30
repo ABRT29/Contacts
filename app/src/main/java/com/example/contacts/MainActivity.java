cf7d5bd22480609911f36bf18663f6ed7aa0a45d
menzo-ko <korchit.menzo@gmail.com>
2020-01-30 08:48:28 +0100

    initialisation

--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -20,7 +20,7 @@ import android.widget.SimpleCursorAdapter;
 public class MainActivity extends AppCompatActivity {
 
     private ContactDbAdapter db;
-
+// COUcou c'est menzo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
